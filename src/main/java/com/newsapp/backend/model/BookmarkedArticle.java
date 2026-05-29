69e47b6f5db8ccdc062fc13e32cd616dc3adb7e2
happyNicky <naoldana5@gmail.com>
2026-05-29 17:15:56 +0300

    first commit

---
+++ src/main/java/com/newsapp/backend/model/BookmarkedArticle.java
@@ -0,0 +1,48 @@
+package com.newsapp.backend.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "bookmarked_articles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class BookmarkedArticle {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(length = 2048)
+    private String id; // We will use the URL hash or original ID as the primary key
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private String category;
+
+    private String author;
+
+    @Column(columnDefinition = "TEXT")
+    private String imageUrl;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private LocalDateTime publishedAt;
+
+    private Integer readTimeMinutes;
+
+    @ManyToMany(mappedBy = "bookmarks", fetch = FetchType.LAZY)
+    @JsonIgnore
+    @Builder.Default
+    private Set<User> users = new HashSet<>();
+}
